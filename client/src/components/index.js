b52fbe6aeb1c46f92b3e5ff11653848d83d0c2f2
ketandayke <ketandayke@gmail.com>
2025-02-21 00:46:47 +0530

    edited index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,10 +1,5 @@
 import Homepage from "../pages/Homepage";
 import HamburgerMenu from "./hamburgerMenu";
 import SearchBar from "./searchBar";
-<<<<<<< HEAD
-
-export {Homepage,HamburgerMenu,SearchBar}
-=======
 import AboutUs from "../pages/about us/act";
 export { Homepage, HamburgerMenu, SearchBar, AboutUs };
->>>>>>> 040ec8d29b4e922fa252489dfab537c563668f61
