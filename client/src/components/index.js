277580c81b01e9d9b1b4cdc18f0594763a4357f7
ketandayke <ketandayke@gmail.com>
2025-02-21 00:55:20 +0530

    commited

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,8 @@ import Homepage from "../pages/Homepage";
 import HamburgerMenu from "./hamburgerMenu";
 import SearchBar from "./searchBar";
 import AboutUs from "../pages/about us/act";
-export { Homepage, HamburgerMenu, SearchBar, AboutUs };
+import Director from "../pages/Director"
+import Chairman from "../pages/Chairman"
+import Registrar from "../pages/Registrar"
+import Staff from "../pages/Staff"
+export { Homepage, HamburgerMenu, SearchBar, AboutUs,Director,Chairman,Registrar,Staff};
