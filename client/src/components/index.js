548c3d5dc7fe706baae67de3a11c714faa44f0ea
ketandayke <ketandayke@gmail.com>
2025-02-22 01:59:58 +0530

    changed file name

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,7 +3,7 @@ import SearchBar from "./searchBar";
 import LeadershipCard from "./LeadershipCard";
 import StaffTable from "./StaffTable";
 import AdminNavbar from "./adminNavbar"
-import Navbar from "./navbar.jsx";
+import Navbar from "./temp.jsx";
 import Hero from "./hero.jsx";
 import Mission from "./mission.jsx";
 import StatsCounter from "./statsCounter.jsx";
